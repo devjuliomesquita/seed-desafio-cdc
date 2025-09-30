af93354a08abde6a09e3b7b048ec90efa650a6f6
juliocesarmcamilo@gmail.com <juliocesarmcamilo@gmail.com>
2025-09-29 23:29:22 -0300

    Configurado banco postgres e banco mongodb ao mesmo tempo

--- cdc/src/main/java/com/juliomesquita/cdc/CdcApplication.java
+++ cdc/src/main/java/com/juliomesquita/cdc/CdcApplication.java
@@ -1,6 +1,5 @@
 package com.juliomesquita.cdc;
 
-import jakarta.transaction.Transactional;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ cdc/src/main/java/com/juliomesquita/cdc/shared/configs/TransactionManagersConfig.java
@@ -0,0 +1,24 @@
+package com.juliomesquita.cdc.shared.configs;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class TransactionManagersConfig {
+
+    @Bean(name = "jpaTransactionManager")
+    public PlatformTransactionManager jpaTransactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+
+    @Bean(name = "mongoTransactionManager")
+    public PlatformTransactionManager mongoTransactionManager(MongoDatabaseFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
+    }
+}
+
